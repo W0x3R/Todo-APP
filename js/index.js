9886480f396d3d60ab3f76582875d177ef3cd448
Stanislau Vyrski <vyr4376@yandex.ru>
2024-01-06 11:14:07 +0300

    refactor: add comments (Sat, Jan 6, 2024, 11:14:01 AM)

--- js/index.js
+++ js/index.js
@@ -12,14 +12,7 @@ const showMessage = () => {
 			<input class = 'inner__list-checkbox' type ='checkbox' id='item_${index}' ${item.checked ? 'checked' : ''}>
 			<label for ='item_${index}'>${item.todoValue}</label>
 			
-			<svg version="1.1" " xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
-			width="30px" height="30px" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve">
-			<line fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" x1="39" y1="43" x2="25" y2="29"/>
-			<line fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" x1="25" y1="43" x2="39" y2="29"/>
-			<polyline fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" points="23,8 10,8 10,63 54,63 54,8 41,8 "/>
-			<polygon fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" points="36,5 36,1 28,1 28,5 24,5 22,13 42,13 40,5 
-			"/>
-			</svg>
+			<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width ='30px' height ='30px' x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"><polygon style="fill:#E21B1B;" points="404.176,0 256,148.176 107.824,0 0,107.824 148.176,256 0,404.176 107.824,512 256,363.824 404.176,512 512,404.176 363.824,256 512,107.824 "/></svg>
 
 		</li>`;
 		list.innerHTML = listMessage
