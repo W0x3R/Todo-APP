088442caa9254cd946aa661fb892ce966a120d9d
Stanislau Vyrski <vyr4376@yandex.ru>
2024-07-29 17:54:30 +0300

    refactor: the orminputplaceholders object has been moved to the new forminputplaceholders module. (Mon, Jul 29, 2024, 5:54:16 PM)

---
+++ src/js/inputField/formInputPlaceholders.js
@@ -0,0 +1,5 @@
+export const formInputPlaceholders = {
+	basic: 'Description',
+	duplication: 'This value already exists',
+	empty: 'The input field is empty.',
+}
--- src/js/inputField/setFormInputPlaceholder.js
+++ src/js/inputField/setFormInputPlaceholder.js
@@ -1,10 +1,5 @@
 import { formInput } from "./focusFormInput"
-
-const formInputPlaceholders = {
-	basic: 'Description',
-	duplication: 'This value already exists',
-	empty: 'The input field is empty.',
-}
+import { formInputPlaceholders } from "./formInputPlaceholders"
 
 export const setFormInputPlaceholder = (value) => {
 	formInput.setAttribute('placeHolder', formInputPlaceholders[value])
