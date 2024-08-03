683ee2516c48cd53e6b03004effc7ed2c60ec994
Stanislau Vyrski <vyr4376@yandex.ru>
2024-08-03 19:23:02 +0300

    fix: fixed a bug that caused the before pseudo element to get the wrong color scheme when switching theme and creating a new tag with form__list-label class. (Sat, Aug 3, 2024, 7:22:56 PM)

--- src/js/changeTheme/setCreateTaskTheme.js
+++ src/js/changeTheme/setCreateTaskTheme.js
@@ -2,5 +2,5 @@ import { getStorageValue } from "../storage/getStorageValue"
 
 export const setCreateTaskTheme = () => {
 	let currentTheme = getStorageValue('theme')
-	return currentTheme === 'light' ? ['', ''] : ['form__list-item_theme-dark', 'form__polygon_theme-dark']
+	return currentTheme === 'light' ? ['', '', ''] : ['form__list-item_theme-dark', 'form__polygon_theme-dark', 'form__list-label_dark-theme']
 }
--- src/js/task/createTaskMarkup.js
+++ src/js/task/createTaskMarkup.js
@@ -6,14 +6,14 @@ export const list = document.querySelector('.form__list');
 
 export const createTaskMarkup = () => {
 	const taskTheme = setCreateTaskTheme()
-	let [liClass, polygonClass] = taskTheme
+	let [liClass, polygonClass, formLabelClass] = taskTheme
 
 	let listMessage = ''
 	todoArr.forEach((el, i) => {
 		listMessage += `
 		<li class ='form__list-item ${liClass}'>
 			<input class = 'form__list-checkbox' type ='checkbox' id='item_${i}' ${el.checked ? 'checked' : ''}>
-			<label class = 'form__list-label'  for ='item_${i}'><span>${el.value}</span></label>
+			<label class = 'form__list-label ${formLabelClass}'  for ='item_${i}'><span>${el.value}</span></label>
 
 			<svg class ='form__delete-list' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><polygon class ='form__polygon ${polygonClass}' points="404.176,0 256,148.176 107.824,0 0,107.824 148.176,256 0,404.176 107.824,512 256,363.824 404.176,512 512,404.176 363.824,256 512,107.824"/></svg>
 
